1a445a3c0d5f61c745341226e85cbd3186da535d
obrezkov <obrezkov@axinom.com>
2023-08-11 17:32:06 +0300

    db.sql added

---
+++ db.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE app_public.video (
+                                  id uuid NOT NULL,
+                                  collection_parent_id uuid NOT NULL,
+                                  season_parent_id uuid,
+                                  section_parent_id uuid,
+                                  next_video_id uuid,
+                                  title text NOT NULL
+);
+
+CREATE TABLE app_public.season (
+                                   id uuid NOT NULL,
+                                   collection_parent_id uuid NOT NULL,
+                                   title text NOT NULL
+);
+CREATE TABLE app_public.collection (
+                                       id uuid NOT NULL,
+                                       title text NOT NULL
+);
